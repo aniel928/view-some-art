83ef7cdad82ee358db7f41ef557873c8ed8d83c1
Anne Whitman <aniel928@gmail.com>
2023-08-09 21:51:02 -0400

    build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [image, updateImage] = useState('');
+    const [searchTerm, updateSearchTerm] = useState('');
+    const searchRef = useRef(null);
+
+    function handleSearch(e) {
+        e.preventDefault();
+        if (searchRef.current.value === searchTerm) {
+
+        }
+        updateSearchResults(searchRef.current.value);
+    }
+    const updateSearchResults = async (searchTerm) => {
+        const url = `https://api.artic.edu/api/v1/artworks/search?q=${searchTerm}&fields=id,title,image_id&limit=100`;
+        const data = await fetch(url);
+        const imgData = await data.json();
+
+        const length = imgData.data.length;
+        const randomImage = imgData.data[Math.floor(Math.random() * length)]
+
+        updateImage([
+            <div>
+                <img
+                    className='artwork'
+                    src={`https://www.artic.edu/iiif/2/${randomImage.image_id}/full/843,/0/default.jpg`}
+                    alt={`${randomImage.title}`}
+                />
+                <p>{randomImage.title}</p>
+            </div>
+        ]);
+    }
+
+    return (
+        <div className="App">
+            <form className="searchForm" onSubmit={(e) => { handleSearch(e) }}>
+                <label>Search for</label>
+                <input
+                    ref={searchRef}
+                    type='text'
+                />
+                <button type='submit'>Go</button>
+            </form>
+            <header className="App-header">
+                {image}
+            </header>
+        </div>
+    );
 }
 
 export default App;
